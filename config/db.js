ef9e1a1b2228da1a0f936d5260ff5ac81de56105
Chintan Savalia <chintan2011@gmail.com>
2019-11-24 04:52:27 -0500

    .gitignore

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// Map global promises
-mongoose.Promise = global.Promise;
-
-// db connect
-mongoose.connect(/* add your mongoose connection here */)
-.then(() => console.log('MongooseDB Connection'))
-.catch(err => console.log(err));
